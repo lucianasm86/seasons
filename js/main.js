cbfdff7a4d3035affcd9aa12b2317c2ae04a240d
Luciana Mendonca <Lu@luciana-mendoncas-macbook.local>
2018-05-07 21:59:39 -0700

    some js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var seasons = [
+	{
+		season: "Winter",
+		months: "December - February",
+		activities: "Skiing, Snowboarding, Having a snowball fights, Making snow angels"
+	},
+	{
+		season: "Spring",
+		months: "March - May",
+		activities: "Going camping, Going to the beach, Flying a kite, Going kayaking"
+	},
+	{
+		season: "Summer",
+		months: "June - August",
+		activities: "Going to the beach, Eating ice cream, Having barbecues, Going Hiking"
+	},
+	{
+		season: "Autumn",
+		months: "September - November",
+		activities: "Going to the mountains, Jumping in a pile of leaves, Carving pumpkins, Building bonfires"
+	}
+]
